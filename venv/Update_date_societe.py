acb17072ffdd8644cdbd2a7a7798df2494b0ed3b
Sabri <glad77fr@yahoo.fr>
2018-05-07 16:08:53 +0200

    new

--- venv/Update_date_societe.py
+++ venv/Update_date_societe.py
@@ -7,7 +7,7 @@ class Imp_soc:
     def __init__(self):
         self.measure = pd.DataFrame
         self.affectation = pd.DataFrame
-        self.result = pd.DataFrame(columns=["Matricule", "Date_debut", "Type_cas"])
+        self.result = pd.DataFrame(columns=["Matricule", "Date_debut"])
 
     def chargement(self, fichier, chemin, feuille=""):
 
@@ -31,7 +31,19 @@ class Imp_soc:
     def transformation(self):
 
         res_aff = pd.DataFrame() # contient le distinct mat/soc/date debut
-        res_aff = self.affectation[['Mat.', 'CSté', 'Date déb.']].drop_duplicates() #Recup des champs utiles dans affectation
+        a = self.affectation
+        val = ["Z","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        a['Test'] = a['Domaine'].apply(lambda x: "ko" if x[1]in val else 'ok')
+        b = a[a.Test == "ok"]
+
+       #a = a.query('Domaine=="A"')
+        #val = ['F2',"F1"]
+        #a = a[a['Domaine'].str.match(val)]
+
+        res_aff = b[['Mat.', 'CSté', 'Date déb.']].drop_duplicates()
+
+
+        #res_aff = self.affectation[['Mat.', 'CSté', 'Date déb.']].drop_duplicates() #Recup des champs utiles dans affectation
         res_aff['Date déb.'] = pd.to_datetime(res_aff['Date déb.'], errors='coerce')
         res_soc = pd.DataFrame() #contient que les val embauche, reembauche
         list_mat = pd.Series
@@ -41,42 +53,22 @@ class Imp_soc:
         res_aff = res_aff.sort_values(by=['Mat.', 'CSté', 'Date déb.'], ascending=False)
 
         res_aff = res_aff.sort_values('Date déb.').groupby(['Mat.', 'CSté'], as_index=False).min()
+        res_aff = res_aff.sort_values('Date déb.').groupby(['Mat.'], as_index=False).max()
 
+        writer = pd.ExcelWriter(r"D:\Users\sgasmi\Desktop\affectation_distinct.xlsx", engine='xlsxwriter')
+        res_aff.to_excel(writer, sheet_name="Res")
+        writer.save()
+        print("Ok")
         res_soc = self.measure
 
-        for i, cel in enumerate(list_mat): # alimentation des resultats
-            self.result.at[i, 'Matricule'] = cel
 
-        for i, mat in enumerate(self.result["Matricule"]):
-            dateref = "vide"
-            date_cible =""
-            if mat in res_aff["Mat."].values:
-                for y, val in enumerate(res_aff.itertuples()):
-                    if self.result.at[i,"Matricule"] == res_aff.at[y,"Mat."]:
-
-                        if dateref != "vide":
-                            if (dateref - res_aff.at[y, "Date déb."]).days > 0:
-                                #date_cible = datetime.strptime(str(res_aff.at[y,"Date déb."]),'%Y-%m-%d %H:%M:%S')
-                                date_cible = datetime.strptime(str(dateref), '%Y-%m-%d %H:%M:%S')
-                        if dateref == "vide":
-                            dateref = datetime.strptime(str(res_aff.at[y,"Date déb."]),'%Y-%m-%d %H:%M:%S')
-
-            self.result.at[i,'Date_debut'] = date_cible
-            self.result.at[i,'Type_cas'] = "2"
-
-
-        for i, mat in enumerate(self.result["Matricule"]):
-            for y, val in enumerate(res_soc.itertuples()):
-
-                if self.result.at[i,'Matricule'] == res_soc.at[y,"Mat."]:
-                    if self.result.at[i,'Date_debut'] == "" and res_soc.at[y, 'Mes.'] =="00":
-                        self.result.at[i,'Date_debut'] =res_soc.at[y,'Date déb.']
-                        break
 
 
 analyse = Imp_soc()
+analyse.chargement("affectation", r"D:\Users\sgasmi\Desktop\Copie de exportv12589.xlsx", "Sheet1")
 analyse.chargement("mesure", r"D:\Users\sgasmi\Desktop\PA0000_2304 (mesure).xlsx", "PA0000_2304")
-analyse.chargement("affectation", r"D:\Users\sgasmi\Desktop\PA0001 (affectation).xlsx", "PA0001")
+#analyse.chargement("affectation", r"C:\Users\Sabri.GASMI\Desktop\Copie de PA0001 (affectation) - Copie.xlsx", "PA0001")
 analyse.transformation()
 
-analyse.exportexcel(r"D:\Users\sgasmi\Desktop\res.xlsx","Resultat")
+#analyse.exportexcel(r"D:\Users\sgasmi\Desktop\res.xlsx","Resultat")
+analyse.exportexcel(r"D:\Users\sgasmi\Desktop\leres.xlsx","Resultat")
